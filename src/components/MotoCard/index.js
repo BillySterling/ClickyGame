da5c8f2a52a3d0576e14d5578a12cbc5605d26cb
Billy Sterling <44212511+BillySterling@users.noreply.github.com>
2019-04-10 16:35:54 -0400

    continuing code development

--- src/components/MotoCard/index.js
+++ src/components/MotoCard/index.js
@@ -1,18 +1,16 @@
 import React from "react";
 import "./style.css";
 
-function MotoCard(props) {
-  return (
+const MotoCard = props => (
     <div 
     className="card"
     value={props.id} 
-    onClick={() => props.handleClick(props.id)}
-    >
-      <div className="img-container">
+    onClick={() => props.handleClick(props.id)}>
+      <div className="cardImage">
         <img alt={props.name} src={props.image} />
       </div>
     </div>
   );
-}
+
 
 export default MotoCard;
